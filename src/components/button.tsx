b18c7df34f95b819bf62c56e114329040bc0d723
Marcos Paulo <marcospaullo552@gmail.com>
2024-05-07 07:26:10 -0300

    fix: components

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,6 +6,7 @@ export function Button({ children, ...props }: ButtonProps) {
   return (
     <button 
     className="bg-green-800 w-full py-2 rounded-lg text-zinc-100 font-bold text-sm hover:bg-green-900"
+    {...props}
     >
       {children}
     </button>
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -2,11 +2,11 @@ import { ComponentProps } from "react"
 
 interface InputProps extends ComponentProps<"input"> { }
 
-export function Input({ ...props }) {
+export function Input({ ...props }: InputProps) {
   return (
     <input 
     className="w-full h-10 bg-transparent border border-zinc-300 px-4 py-2 rounded-lg text-zinc-800 font-medium"
     {...props}
     />
   )
-}
+} 
